8701b263bbbc09ea3108fafc3c4e13bd04901eb7
scott-wyatt <scottwyatt86@gmail.com>
2016-12-18 13:18:25 -0500

    Init

---
+++ errors/index.js
@@ -0,0 +1,4 @@
+exports.ConflictError = require('./conflict')
+exports.ValidationError = require('./validation')
+exports.FoundError = require('./found')
+exports.ModelError = require('./model')
---
+++ index.js
@@ -0,0 +1,1 @@
+exports.Errors = require('./errors')
